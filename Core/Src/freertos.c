d9d85cccd2bc5dd53104dd45c3cba60417598a4f
CNLHC <2463765697@qq.com>
2020-01-11 20:04:19 +0800

    new freertos settings

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -81,6 +81,22 @@ void FFT_task(void const * argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
+/* GetIdleTaskMemory prototype (linked to static allocation support) */
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize );
+
+/* USER CODE BEGIN GET_IDLE_TASK_MEMORY */
+static StaticTask_t xIdleTaskTCBBuffer;
+static StackType_t xIdleStack[configMINIMAL_STACK_SIZE];
+  
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize )
+{
+  *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
+  *ppxIdleTaskStackBuffer = &xIdleStack[0];
+  *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+  /* place for user code */
+}                   
+/* USER CODE END GET_IDLE_TASK_MEMORY */
+
 /**
   * @brief  FreeRTOS initialization
   * @param  None
@@ -153,10 +169,10 @@ void MX_FREERTOS_Init(void) {
 /* USER CODE END Header_StartDefaultTask */
 void StartDefaultTask(void const * argument)
 {
-    
-    
-    
-    
+
+
+
+
 
   /* USER CODE BEGIN StartDefaultTask */
   /* Infinite loop */
@@ -195,7 +211,7 @@ void adStartSample(void const * argument)
 
 			while(AD7606_BUSY);
 			AD7606_CS_L;
-			
+
 			HAL_SPI_Receive_IT(&hspi1,(uint8_t * )buf,8);
 		}
 	}
